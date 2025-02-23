72436fec8f5529c2e60142f61bf65029a9443ced
Gooey64 <61029550+Gooey64@users.noreply.github.com>
2025-02-23 00:50:35 -0500

    Added functionality to take a port other than localhost3000

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const uri = "mongodb+srv://dbUser:HkL67mVXTSwL7jki@myfridgepal.ngs96.mongodb.net/?retryWrites=true&w=majority&appName=MyFridgePal";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 app.use(express.json());
